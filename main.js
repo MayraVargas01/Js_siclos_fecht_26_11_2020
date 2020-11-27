6a750418c221613cfd0ca0b27ead01e404e224da
Marìa Carolina Gòmez <mayravargase01@gmail.com>
2020-11-26 19:41:45 -0500

    Js_siclos_fecht_26_11_2020

---
+++ main.js
@@ -0,0 +1,28 @@
+let numero = 1;
+do{
+    numero++;
+    console.log(numero);
+}while(numero=3);
+
+if(numero>5);
+
+for(let i = 1; i < 3; i++) {
+    console.log(i)
+}
+
+for(let i = 1; i > 6; i--) {
+    console.log(i)
+}
+
+let lista = ["gato, perros, gallina. elefante, delfin"];
+for (let i =0; i < lista.length; i++) {
+    console.log(lista[i]);
+}
+
+for(dato of lita){
+    console.log(dato);
+}
+
+for(posicion in lita){
+    console.log(posicion);
+}
